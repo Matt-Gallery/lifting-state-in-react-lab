b77f0ff0b417d8c3b93f9f0ca0278157135fd96e
matt-gallery <matt.r.gallery@gmail.com>
2025-04-11 16:44:23 -0400

    main functionality working. Need to fix styling.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
+import IngredientList from './components/IngredientList/IngredientList';
+import BurgerStack from './components/BurgerStack/BurgerStack';
 
 const App = () => {
-  const availableIngredients = [
+  const [availableIngredients, setAvailableIngredients] = useState([
     { name: 'Kaiser Bun', color: 'saddlebrown' },
     { name: 'Sesame Bun', color: 'sandybrown' },
     { name: 'Gluten Free Bun', color: 'peru' },
@@ -19,13 +21,24 @@ const App = () => {
     { name: 'Onion', color: 'lightyellow' },
     { name: 'Cheddar Cheese', color: '#FDE18B' },
     { name: 'Swiss Cheese', color: '#F1E1A8' },
-  ];
+  ]);
+
+  const [stack, setStack] = useState([]);
+
+  const addToBurger = (ingredient) => {
+    setStack([...stack, ingredient]);
+  };
+
+  const removeFromBurger = (index) => {
+    setStack(stack.filter((_, i) => i !== index));
+  };
 
   return (
     <main>
       <h1>Burger Stacker</h1>
       <section>
-      {/* List & Stack components */}
+        <IngredientList ingredients={availableIngredients} addToBurger={addToBurger} />
+        <BurgerStack stack={stack} removeFromBurger={removeFromBurger} />
       </section>
     </main>
   );
--- src/components/BurgerStack/BurgerStack.jsx
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -1,8 +1,21 @@
 // src/components/BurgerStack/BurgerStack.jsx
 
 const BurgerStack = (props) => {
-    return <ul>// map through props.ingredients</ul>;
-  };
-  
-  export default BurgerStack;
+  return (
+    <ul>
+      {props.stack.length === 0 ? (
+        <li>Burger is empty!</li>
+      ) : (
+        props.stack.map((ingredient, index) => (
+          <li key={index} style={{ backgroundColor: ingredient.color }}>
+            {ingredient.name}
+            <button onClick={() => props.removeFromBurger(index)}>X</button>
+          </li>
+        ))
+      )}
+    </ul>
+  );
+};
+
+export default BurgerStack;
   
